e6acb792075f64ae1173dc4811b4d16d619c52f4
CorbicBojana <corbicbojana@gmail.com>
2020-05-06 12:02:46 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
-// JS
+var navBtn = document.getElementById("nav-btn");
+var navList = document.getElementById("nav-list");
+
+navBtn.addEventListener("click", function() {
+    if (navBtn.classList.contains("close-nav")) {
+        navBtn.classList.remove("close-nav");
+        navBtn.classList.add("open-nav");
+        navList.classList.add("open-nav-list");
+        navList.style.display = "flex"
+    } else {
+        navBtn.classList.remove("open-nav");
+        navBtn.classList.add("close-nav");
+        navList.classList.remove("open-nav-list");
+        navList.style.display = "none"
+    }
+})
